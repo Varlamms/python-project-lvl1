4078987e11fc2bccf786e4340506cb8ccbde81d1
Varlamms <Varlamovevgenii@gmail.com>
2022-07-21 18:12:00 +0400

    update 21.07

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,19 +1,17 @@
 from random import randint
 def main():
-    q = randint(0, 555)
-    print('Answer "yes" if the number is even, otherwise answer "no".')
+    print ('Answer "yes" if the number is even, otherwise answer "no".')
+    q = randint (0, 555)
     if q % 2 == 0:
         correct_answer = "'yes'"
     else:
         correct_answer = "'no'"
-    print('Question: ' + str(q))
-    answer = input('Your answer: ')
-    if answer == 'yes' and q % 2 == 0 or answer == 'no' and q % 2 > 0:
-        print('Correct!')
-        return True
-    else:
-        print("'" + answer + "'" + ' is wrong answer ;(.' + ' Correct answer was ' + correct_answer + ".\nLet's try again")
-        return False
-
-if __name__ == '__main__':
-    main()
+    for _ in range(3):
+        q = randint(0, 555)
+        print('Question: '+ str(q))
+        answer = input('Your answer: ')
+        if answer == 'yes' and q % 2 == 0 or answer == 'no' and q % 2 > 0:
+            print('Correct')
+        else:
+            print("'" + answer + "'" + ' is wrong answer ;(.' + ' Correct answer was ' + correct_answer + ".\nLet's try again, name")
+            break
